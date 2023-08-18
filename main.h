92303a1bebc68d4f1a00d3e91804dea9e7e53e51
hadjara <hadjaramamane61@gmail.com>
2023-08-18 06:30:32 -0700

    Files for our version of printf

--- main.h
+++ main.h
@@ -24,34 +24,26 @@ int op_cent(va_list note);
 int _printf(const char *format, ...);
 int display(char c);
 int (*ob_func(char a))(va_list note);
+int op_bi(va_list);
+int op_rot(va_list);
+int op_unsigned(va_list);
+int op_octal(va_list);
+int op_hexadecimal_low(va_list);
+int op_hexadecimal_upp(va_list);
+int op_pointer(va_list);
+int op_rev_string(va_list);
 
 /* utils.c */
-int _strlen(const char *);
+int _strcmp(char *str1, char *str2);
 int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
+char *op_conv(long int, int);
 
 /* handler.c */
 int handler(const char *, va_list);
 int percent_handler(const char *, va_list, int *);
 
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-/* _putchar.c */
+/* _putchar.c
 int _putchar(char);
-int buffer(char);
+int buffer(char);*/
 
 #endif /* MAIN_H */
